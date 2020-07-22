db75adf21927db4eca82da9f87186f72228b58d0
MakarCodes <jarek.strupek@wp.pl>
2020-07-22 15:47:41 +0200

    Updates

---
+++ precache-manifest.8455037bea4b733efa96f1854b694fae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157cc01a29797c9c564f2c57ba6fc063",
+    "url": "/Pokedex_react/index.html"
+  },
+  {
+    "revision": "78b4684eee75555b586a",
+    "url": "/Pokedex_react/static/css/main.0c7c25b2.chunk.css"
+  },
+  {
+    "revision": "dd295f11067f59d8c452",
+    "url": "/Pokedex_react/static/js/2.e7a9a969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokedex_react/static/js/2.e7a9a969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b4684eee75555b586a",
+    "url": "/Pokedex_react/static/js/main.187f2ff8.chunk.js"
+  },
+  {
+    "revision": "593da696fa00e5b36af6",
+    "url": "/Pokedex_react/static/js/runtime-main.7a9ad61c.js"
+  },
+  {
+    "revision": "0bda2da2153a00edb2b67f0ac5103307",
+    "url": "/Pokedex_react/static/media/Pokemon-LOGO.0bda2da2.png"
+  }
+]);
